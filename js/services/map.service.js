a617483001f0e3877ebd492b81d1852d6bcf7eda
Maor Aknin <maoraknin125@gmail.com>
2022-12-21 15:37:17 +0200

    design and map starter

---
+++ js/services/map.service.js
@@ -0,0 +1,107 @@
+export const mapService = {
+    initMap,
+    addMarker,
+    panTo,
+    // mapClick
+}
+
+
+// Var that is used throughout this Module (not global)
+var gMap
+
+function initMap(lat = 32.0749831, lng = 34.9120554) {
+    console.log('InitMap')
+    return _connectGoogleApi()
+        .then(() => {
+            console.log('google available')
+            gMap = new google.maps.Map(
+                document.querySelector('.map-container'), {
+                center: { lat, lng },
+                zoom: 15
+            })
+            console.log('Map!', gMap)
+
+            let infoWindow = new google.maps.InfoWindow({
+                content: "Click the map to get Lat/Lng!",
+                position: { lat, lng },
+            });
+        
+            infoWindow.open(gMap);
+        
+            gMap.addListener("click", (mapsMouseEvent) => {
+                infoWindow.close();
+                infoWindow = new google.maps.InfoWindow({
+                    position: mapsMouseEvent.latLng,
+                });
+            
+                infoWindow.setContent(
+                    JSON.stringify(mapsMouseEvent.latLng.toJSON(), null, 2)
+                );
+            
+            
+                infoWindow.open(gMap);
+                // gCurrCords = JSON.parse(infoWindow.content)
+                // marker.position = gCurrCords
+                // mapClick()
+            });
+        })
+}
+
+
+function addMarker(loc) {
+    var marker = new google.maps.Marker({
+        position: loc,
+        map: gMap,
+        title: 'Hello World!'
+    })
+    return marker
+}
+
+function panTo(lat, lng) {
+    var laLatLng = new google.maps.LatLng(lat, lng)
+    gMap.panTo(laLatLng)
+}
+
+
+function _connectGoogleApi() {
+    if (window.google) return Promise.resolve()
+    const API_KEY = '' //TODO: Enter your API Key
+    var elGoogleApi = document.createElement('script')
+    elGoogleApi.src = `https://maps.googleapis.com/maps/api/js?key=AIzaSyBody3Gk1JTzeJQpy_p-uQuN-mvnxarwOM`
+    elGoogleApi.async = true
+    document.body.append(elGoogleApi)
+
+    return new Promise((resolve, reject) => {
+        elGoogleApi.onload = resolve
+        elGoogleApi.onerror = () => reject('Google script failed to load')
+    })
+}
+
+
+
+
+
+
+// function mapClick() {
+//     var name = prompt('please describe location')
+
+   
+
+//     let date = new Date();
+//     let current_date = date.getDate() + "/" + (date.getMonth() + 1) + "/" + date.getFullYear();
+//     let current_time = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+//     let date_time = current_date + " " + current_time;
+
+//     if (!name) name = 'elhanan'
+//     const chosenLocation = {
+//         name,
+//         id: makeId(),
+//         time: date_time,
+//         lat: gCurrCords.lat,
+//         lng: gCurrCords.lng
+//     }
+
+//     gSavedLocations.push(chosenLocation)
+//     saveToStorage(LOCATION_KEY, gSavedLocations)
+
+// }
